a0dd85e124f43df49318810acaec9d262b231261
Balram Rajbanshi <balramrajbanshi4769@gmail.com>
2025-08-03 22:28:35 +0545

    authentication

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -110,36 +110,35 @@
 
 
 
-require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const JWT_SECRET = process.env.JWT_SECRET;
-const { promisify } = require("util");
+const JWT_SECRET = process.env.JWT_SECRET
+const {promisify} = require("util");
 const User = require("../model/userModel");
+const isAuthenticated = async(req,res,next)=>{
 
-const isAuthenticated = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
+    const token = req.headers.authorization?.split(' ')[1]; 
+       if (!token) {
+           return res.status(403).json({ message: "Please login" });
+       }
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(403).json({ message: "Please login" });
-  }
+    // alternative waY second
+   try {
+     const decoded = await promisify(jwt.verify)(token,JWT_SECRET);
+    //  console.log("decoded",decoded);
+     
+    const doesUserExist = await User.findOne({_id:decoded.id});    
+    if(!doesUserExist){
+       return res.status(403).json({
+            message:"User doesnot exist with that token/id"
+        })
+    } 
+    req.user = doesUserExist    
+    next()
+   } catch (error) {
+    res.status(400).json({
+        message:error.message
+    })
+   }   
+}
 
-  const token = authHeader.split(" ")[1];
-
-  try {
-    const decoded = await promisify(jwt.verify)(token, JWT_SECRET);
-    const doesUserExist = await User.findById(decoded.id);
-
-    if (!doesUserExist) {
-      return res.status(403).json({ message: "User does not exist with that token/id" });
-    }
-
-    req.user = doesUserExist;
-    next();
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-};
-
-module.exports = isAuthenticated;
-
-
+module.exports = isAuthenticated
